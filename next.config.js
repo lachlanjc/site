6ce828b34808f037c2cc0bcae31695095f57d611
Lachlan Campbell (they/them) <lachlanjc@hey.com>
2022-11-18 16:09:41 -0500

    Add Mastodon webfinger

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 const withMDX = require('@next/mdx')()
-module.exports = withMDX({
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = withMDX({
   pageExtensions: ['js', 'mdx'],
-  images: { domains: ['cdn.glitch.com'] }
+  images: { domains: ['cdn.glitch.com'] },
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/webfinger',
+        destination:
+          'http://mastodon.social/.well-known/webfinger?resource=acct:lachlanjc@mastodon.social',
+      },
+    ]
+  },
 })
+
+module.exports = config
